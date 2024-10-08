fed6078d335ca98f0b9a9f58b1984d05b3fea37e
blaxlistZ <blaxlistz@gmail.com>
2024-10-08 15:25:25 +0800

    "updated comments."

--- src/C/main.c
+++ src/C/main.c
@@ -1,7 +1,7 @@
 #include "bbst.h"
 #include <stdio.h>
 
-// DO NOT EVER BUILD WITH THIS SETTINGS
+// DO NOT EVER BUILD AND RUN WITH THIS SETTINGS
 #define SHIFT_SCALE 63  // Maximum shift scale for bit shifting (Same as BIT_MAX)
 #define FILE_SIZE 13514 // 233 Character Capacity * 58 Unrestricted ASCII. Much better solution is Implementation of Combinations.
 
